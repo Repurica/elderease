f6fe529b0ab2cde0852e4cfc60f6bf6596cb4b8c
Isis Eka Kesuma <isis.ek.2023@scis.smu.edu.sg>
2024-08-31 13:31:10 +0800

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import dialog from 'primevue/dialog'
-import '@fortawesome/fontawesome-free/css/all.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
 </script>
 
 <template>
